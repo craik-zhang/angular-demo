91fcadc939f4f1e6d4ea826c20e733dfc68db2fb
chenpengfei <alffei@163.com>
2020-04-18 18:32:43 +0800

    feat:sign in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SignInComponent} from './pages/sign-in/sign-in.component';
+import {ProductComponent} from './pages/product/product.component';
+import {ProductDetailComponent} from './pages/product-detail/product-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'products', component: ProductComponent},
+  { path: 'product/:id', component: ProductDetailComponent}
+];
 
 @NgModule({
+  // Registering the RouterModule.forRoot() in the AppModule imports makes the Router service available everywhere in the application.
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
